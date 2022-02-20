f446571afde6b0331640b692c5f360b4346b1b09
osoese <ftriantos@gmail.com>
2022-02-20 07:34:54 -0500

    began a functional web3 design in react shift

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -6,10 +6,12 @@ export default function About() {
   return (
     <>
       <main>
-        <h2>About page clicked</h2>
+        <h2>About: Functional Design</h2>
         <p>
-          You can set dark and light mode icon (stles don't adjust yet) and verify state is changed (the ocon adjusts).
-          If you refresh the page you can verify that local storage is being used to persist
+          You can set dark and light mode icon and verify state is changed.
+          If you refresh the page you can verify that local storage is being used to persist.
+          Right now I am adjusting component styles and their appearance under reactive state changes.
+          I can see why designers make a UI kit.
         </p>
         <Mode />
       </main>
