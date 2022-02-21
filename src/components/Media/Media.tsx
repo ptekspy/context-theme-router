926c16497ce86420df2305dc8e835e4ae852a057
Patrick Kenneally <36610246+ptekspy@users.noreply.github.com>
2022-02-21 15:03:54 +0000

    Updated to be more dynamic and support audio

--- src/components/Media/Media.tsx
+++ src/components/Media/Media.tsx
@@ -2,53 +2,49 @@ import React, { useState, useEffect } from 'react';
 import './Media.css';
 const defaultImage = require('../../logo192.png')
 
-export default function Media(props:any) {
+export default function Media(props: any) {
   const [isLoading, setIsLoading] = useState(true)
   const [content, setContent] = useState(defaultImage);
   const [contentType, setContentType] = useState('none')
+  const SUPPORTED_MEDIA_FORMATS = ['image', 'audio', 'video']
 
   useEffect(() => {
-    if(isLoading === true){
+    if (isLoading === true) {
       fetch(props.url).then(response => response.blob()).then(async blob => {
         console.log(`here`)
-        if(blob.type.split("/")[0] === 'image'){
-          let url = URL.createObjectURL(blob);
-          setContentType('image');
+        if (SUPPORTED_MEDIA_FORMATS.includes(blob.type.split("/")[0])) {
+          const url = URL.createObjectURL(blob);
+          setContentType(blob.type.split("/")[0]);
           setContent(url);
-        }else if(blob.type.split("/")[0] === 'video'){
-          let url = URL.createObjectURL(blob);
-          setContentType('video');
-          setContent(url);
-        }else{
+        } else {
           console.log(`blob.type ${blob.type}`)
         }
       });
       setIsLoading(false);
     }
-  })
+  }, [])
 
   return (
-        <>
-          <div className="media-viewer">
-            <div>
-                {(contentType === 'image')?
-                  <img className="image-media" src={content} />
-                  :
-                    (contentType === 'video')?
-                    <video
-                      className="video-media"
-                      src={content}
-                      autoPlay
-                      muted
-                      controls
-                      loop
-                      />
-                    :
-                    <span>nothing</span>
-                }
-                <span>{contentType}</span>
-            </div>
-          </div>
-        </>
+    <>
+      <div className="media-viewer">
+        <div>
+          {contentType === 'image' ?
+            <img className="image-media" src={content} alt=""/>
+            : contentType === 'video' ?
+              <video
+                className="video-media"
+                src={content}
+                autoPlay
+                muted
+                controls
+                loop
+              />
+              :
+              <span>nothing</span>
+          }
+          <span>{contentType}</span>
+        </div>
+      </div>
+    </>
   );
 }
